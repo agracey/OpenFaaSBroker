5515bc76852f937e09879c5e4e973a092a1fb364
Andrew Gracey <agracey@gmail.com>
2019-07-19 18:46:50 -0700

    catalog mostly done

--- src/api/catalog.js
+++ src/api/catalog.js
@@ -1,43 +1,70 @@
-const catalogHandler = async(req, res) => {
+
+const storeUrl = 'https://raw.githubusercontent.com/openfaas/store/master/functions.json';
+const fetch = require("node-fetch");
+
+
+const buildSchema = (f) => {
+
+
+  const envOptionsSchema = Object.keys(f.environment||{})
+    .reduce((acc, curr)=>({...acc,[curr]:{type:"string"}}),{})
+
+  const schema = {
+    type:"object",
+    properties: {
+      environment:{type:"object", properties:envOptionsSchema}
+    }
+  }
 
   return {
-    services: [
-      {
-        name:"Example Service",
-        id: "Example",
-        description: "A Service to show how OSBA Works",
-        tags: ["example"],
-        requires: [],
-        bindable: true,
-        instances_retrievable: true,
-        bindings_retrievable: true,
-        allow_context_updates: true,
-        metadata:{},
-        dashboard_client:{
-          id:'blah',
-          secret:'faas',
-          redirect_uri:'http://suse.com'
-        },
-        plan_updateable: true,
-        plans: [
-          {
-            id: 'planid',
-            name: 'plan name',
-            description:'Sample Plan',
-            free:true,
-            bindable: true,
-            schemas:{},
-            maximum_polling_duration:30,
-            maintenance_info:{
-              version: '1',
-              description:'mi desc'
-            }
-          }
-        ]
-
-      }
-    ]
+    service_instance:{
+      create:schema,
+      update:schema
+    },
+    service_binding:{
+      create:schema
+    }
   }
+}
+
+const buildPlansForArch = (f) => {
+  return Object.keys(f.images).map((arch)=>({
+    id: arch,
+    name: arch,
+    description:'Run Function in Arch: ' + arch,
+    free:true,
+    bindable: true,
+    schemas:buildSchema(f),
+    maximum_polling_duration:30,
+    maintenance_info:{
+      version: '1',
+      description:'mi desc'
+    }
+  }))
+}
+
+const transformFunction = (f) => {
+  return {
+    name: f.title,
+    id: f.name, //TODO: GUID?
+    description:f.description,
+    tags: [ "Function" , ...Object.keys(f.images)],
+    bindable: true,
+    instances_retrievable: true,
+    bindings_retrievable: true,
+    allow_context_updates: false,
+    plan_updateable: true,
+    plans: buildPlansForArch(f)
+
+  }
+}
+
+const catalogHandler = async(req, res) => {
+
+const data = await fetch(storeUrl).then(res=>(res.json()))
+
+return {services:data.functions.map(transformFunction)}
+
 }
 
 
